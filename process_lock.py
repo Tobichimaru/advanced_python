d4b2eac3c6d18d5e933154e62df62abef3718cf4
Kate <kzabelova@gmail.com>
2018-10-28 09:42:14 +0300

    Make script terminate after process is finished

--- process_lock.py
+++ process_lock.py
@@ -1,12 +1,12 @@
-from multiprocessing import Process, Value, Lock
+import multiprocessing
 
 
 def odd(curr, end, lock):
     while True:
-        if int(curr.value) % 2 == 1:
+        if curr.value > end:
+            return
+        if int(curr.value) % 2:
             lock.acquire()
-            if curr.value > end:
-                break
             print(curr.value)
             curr.value += 1
             lock.release()
@@ -14,22 +14,20 @@ def odd(curr, end, lock):
 
 def even(curr, end, lock):
     while True:
-        if int(curr.value) % 2 == 0:
+        if curr.value > end:
+            return
+        if not int(curr.value) % 2:
             lock.acquire()
-            if curr.value > end:
-                break
             print(curr.value)
             curr.value += 1
             lock.release()
 
 
 if __name__ == '__main__':
-    num = Value('i', 1)
-    lock = Lock()
+    num = multiprocessing.Value('i', 1)
+    lock = multiprocessing.Lock()
 
-    odd_process = Process(target=odd, args=(num, 100, lock))
-    even_process = Process(target=even, args=(num, 100, lock))
+    odd_process = multiprocessing.Process(target=odd, args=(num, 100, lock))
+    even_process = multiprocessing.Process(target=even, args=(num, 100, lock))
     odd_process.start()
     even_process.start()
-    odd_process.join()
-    even_process.join()
